8f1f7c3159b520822865a3c6b653d0188449cec6
sunwen <sunwen.me@qq.com>
2026-01-08 22:48:07 +0800

    Convert package to ROS 2

--- src/src/trajectorysave.cpp
+++ src/src/trajectorysave.cpp
@@ -1,8 +1,10 @@
-#include<iostream>
-#include<fstream>
-#include<eigen3/Eigen/Eigen>
-#include<ros/ros.h>
-#include<tf/transform_listener.h>
+#include <fstream>
+#include <iostream>
+
+#include <rclcpp/rclcpp.hpp>
+#include <tf2/exceptions.h>
+#include <tf2_ros/buffer.h>
+#include <tf2_ros/transform_listener.h>
  
 using namespace std;
  
@@ -12,37 +14,40 @@ ofstream foutC;
 int main(int argc, char **argv){
     //string file_name;
     //ros::param::get("file_name",file_name);
-    ros::init(argc, argv, "save_traj_as_tum");
-    ros::NodeHandle nh;
+    rclcpp::init(argc, argv);
+    auto node = rclcpp::Node::make_shared("save_traj_as_tum");
     
     foutC.open("./test.txt");
-    tf::TransformListener listener;//在ROS中，TransformListener 用于接收和处理变换（transform）信息，它可以监听并处理来自TF库（Transform Library）的变换信息
-    ros::Rate rate(20);
-    while(ros::ok()){
-        tf::StampedTransform transform;//表示带有时间戳的坐标变换信息
+    tf2_ros::Buffer tf_buffer(node->get_clock());
+    tf2_ros::TransformListener listener(tf_buffer);//在ROS中，TransformListener 用于接收和处理变换（transform）信息，它可以监听并处理来自TF库（Transform Library）的变换信息
+    rclcpp::Rate rate(20.0);
+    while(rclcpp::ok()){
         try
         {
             //等待获取从 "/odom" 到 "/base_link" 的坐标变换信息
-            listener.waitForTransform("/camera_init", "/aft_mapped", ros::Time(0), ros::Duration(1));
             //获取从 "/odom" 到 "/base_link" 的坐标变换信息，并将其存储在 transform 变量中
-            listener.lookupTransform("/camera_init", "/aft_mapped", ros::Time(0), transform);
-            foutC << transform.stamp_ << " ";//变换时间戳 transform.stamp_ 写入到输出流 foutC 中
-            float x = transform.getOrigin().getX();
-            float y = transform.getOrigin().getY();
-            float z = transform.getOrigin().getZ();
-            float qx = transform.getRotation().getX();
-            float qy = transform.getRotation().getY();
-            float qz = transform.getRotation().getZ();
-            float qw = transform.getRotation().getW();
-            ROS_INFO("%f %f %f %f %f %f %f",x,y,z,qx,qy,qz,qw);
+            auto transform = tf_buffer.lookupTransform(
+                "camera_init",
+                "aft_mapped",
+                tf2::TimePointZero);
+            foutC << rclcpp::Time(transform.header.stamp).seconds() << " ";//变换时间戳 transform.stamp_ 写入到输出流 foutC 中
+            float x = transform.transform.translation.x;
+            float y = transform.transform.translation.y;
+            float z = transform.transform.translation.z;
+            float qx = transform.transform.rotation.x;
+            float qy = transform.transform.rotation.y;
+            float qz = transform.transform.rotation.z;
+            float qw = transform.transform.rotation.w;
+            RCLCPP_INFO(node->get_logger(), "%f %f %f %f %f %f %f", x, y, z, qx, qy, qz, qw);
             foutC << x <<" " << y << " " << z << " " << qx << " " << qy << " " << qz << " " << qw << std::endl;
         }
-        catch(const std::exception& e)
+        catch(const tf2::TransformException& e)
         {
-            std::cerr << e.what() << '\n';
+            RCLCPP_WARN(node->get_logger(), "%s", e.what());
         }
         rate.sleep();
     }
     foutC.close();
+    rclcpp::shutdown();
     return 0;
 }
